503c3067a508fd35a3f9a3f815c53bca95d9b4b6
HazemAhmedSaad <zozahmed939@gmail.com>
2025-12-13 21:06:21 +0200

    upload landing section

---
+++ src/components/header/header.js
@@ -0,0 +1,65 @@
+import logo from "../../assets/log.png";
+import { Link, NavLink } from "react-router-dom";
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header container pt-4">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-transparent text-white">
+        <div className="container-fluid">
+          {/* Logo */}
+          <Link className="navbar-brand  logo" to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-center w-100">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">
+                  Home
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/features">
+                  Features
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about">
+                  About
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/platform">
+                  Platform
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/testimonials">
+                  Testimonials
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/landing/landing.js
@@ -0,0 +1,39 @@
+import Header from "../header/header";
+import "./Landing.css";
+const Landing = () => {
+  return (
+    <div className="landing ">
+      <Header />
+      <div className="center-element container text-white text-center">
+        <div className="landing-label">
+          <button className="pill-label">Voice-First, Business-Ready</button>
+        </div>
+
+        <div className="landing-content">
+          <h1 className="landing-title text-whit">
+            Deliver instant AI-powered voice experiences to your customers
+          </h1>
+          <p className="landing-subtitle">
+            Nouf is an AI Voice Agent that lets your users order, ask, or
+            interact with your service using natural conversation — fully
+            integrated with your platform in minutes.
+          </p>
+        </div>
+        <div className="landing-buttons d-flex justify-content-center gap-3 mt-4">
+          <button className="btn  btn-light btn-lg get-started-btn rounded-pill">
+            Get Started {">"}
+          </button>
+        </div>
+      </div>
+      <div className="landing-image">
+            <img src={require("../../assets/Clip1.png")} className="clip1" alt="Phone Image" />
+            <img src={require("../../assets/Clip2.png")}  className="clip2" alt="Phone Image" />
+
+            <img src={require("../../assets//Bubble.png")}  className="bubble" alt="Chat Bubble" />
+            <img src={require("../../assets/Iphon.png")}  className="iphone"  alt="Phone Image" />
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
