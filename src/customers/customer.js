e84dfa1cd43b6396acd02f058b2a09ca909ad00d
HazemAhmedSaad <zozahmed939@gmail.com>
2025-12-14 13:40:37 +0200

    edit some parts in css and remove duplicate code

--- src/customers/customer.js
+++ src/customers/customer.js
@@ -1,6 +1,70 @@
+import { useState } from "react";
 import "./customer.css";
 
 const Customers = () => {
+const [techData] = useState([
+    {
+      id: 1,
+      icon: require("../../src/assets/cards/ic1.png"),
+      title: "Plug-and-Play API",
+      desc: (
+        <>
+          <span>Integrate Nouf </span> in minutes, no complex setup. Works
+          seamlessly with web and mobile apps instantly.
+        </>
+      ),
+    },
+    {
+      id: 2,
+      icon: require("../../src/assets/cards/ic2.png"),
+      title: "Accurate Speech Recognition",
+      desc: (
+        <>
+          <span>Understands </span> Arabic & English clearly for every customer
+        </>
+      ),
+    },
+    {
+      id: 3,
+      icon: require("../../src/assets/cards/ic3.png"),
+      title: "Intent-Based AI",
+      desc: (
+        <>
+          <span>Recognizes </span> what users mean, not just the words they say
+        </>
+      ),
+    },
+    {
+      id: 4,
+      icon: require("../../src/assets/cards/ic4.png"),
+      title: "Enterprise-Grade Security",
+      desc: (
+        <>
+          <span>Protects </span> user data with full encryption and compliance
+        </>
+      ),
+    },
+    {
+      id: 5,
+      icon: require("../../src/assets/cards/ic5.png"),
+      title: "Easy Customization",
+      desc: (
+        <>
+          <span>Integrate Nouf </span> in minutes, no complex setup.
+        </>
+      ),
+    },
+    {
+      id: 6,
+      icon: require("../../src/assets/cards/ic6.png"),
+      title: "Advanced Analytics",
+      desc: (
+        <>
+          <span>Track </span> interactions, engagement, and conversions easily
+        </>
+      ),
+    },
+  ]);
   return (
     <div className="customers mt-1 container mb-5">
       <p className="head-title text-center">
@@ -39,135 +103,32 @@ const Customers = () => {
       <div className="tech mt-5">
         <h2 className="text-center mb-4 text-white">Features & Tech</h2>
         <div className="row text-white row-cols-1 row-cols-md-2 row-cols-lg-3">
-          <div className="col">
-            <div className="tech-card d-flex flex-column align-items-center text-center p-4">
-              <div className="img1">
-                <img
-                  src={require("../../src/assets/cards/Group.png")}
-                  className="group"
-                  alt="Tech 1"
-                />
-                <div className="bgimg1">
-                  <img
-                    src={require("../../src/assets/cards/ic1.png")}
-                    alt="Tech 1"
-                  />
+          {techData.map((tech) => (
+            <div className="col">
+              <div className="tech-card-grand">
+                <div className="tech-card-parnt">
+                  <div className="tech-card d-flex flex-column align-items-center text-center p-4">
+                    <div className="img1">
+                      <img
+                        src={require("../../src/assets/cards/Group.png")}
+                        className="group"
+                        alt="Tech 1"
+                      />
+                      <div className="bgimg1">
+                        <img
+                          src={tech.icon}
+                          alt={tech.title}
+                        />
+                      </div>
+                    </div>
+                    <h5 className="m-2">{tech.title}</h5>
+                    <p className="tech-text">{tech.desc}</p>
+                  </div>
                 </div>
               </div>
-              <h5 className="m-2 ">Plug-and-Play API</h5>
-              <p className="tech-text">
-                <span>Integrate Nouf </span> in minutes, no complex setup. Works
-                seamlessly with web and mobile apps instantly.
-              </p>
             </div>
-          </div>{" "}
-          <div className="col">
-            <div className="tech-card d-flex flex-column align-items-center text-center p-4">
-              <div className="img1">
-                <img
-                  src={require("../../src/assets/cards/Group.png")}
-                  className="group"
-                  alt="Tech 1"
-                />
-                <div className="bgimg1">
-                  <img
-                    src={require("../../src/assets/cards/ic2.png")}
-                    alt="Tech 1"
-                  />
-                </div>
-              </div>
-              <h5 className="m-2 ">Accurate Speech Recognition</h5>
-              <p className="tech-text">
-                <span>Understands </span> Arabic & English clearly for every
-                customer
-              </p>
-            </div>
-          </div>{" "}
-          <div className="col">
-            <div className="tech-card d-flex flex-column align-items-center text-center p-4">
-              <div className="img1">
-                <img
-                  src={require("../../src/assets/cards/Group.png")}
-                  className="group"
-                  alt="Tech 1"
-                />
-                <div className="bgimg1">
-                  <img
-                    src={require("../../src/assets/cards/ic3.png")}
-                    alt="Tech 1"
-                  />
-                </div>
-              </div>
-              <h5 className="m-2 ">Intent-Based AI</h5>
-              <p className="tech-text">
-                <span>Recognizes</span> what users mean, not just the words they
-                say
-              </p>
-            </div>
-          </div>
-          <div className="col">
-            <div className="tech-card d-flex flex-column align-items-center text-center p-4">
-              <div className="img1">
-                <img
-                  src={require("../../src/assets/cards/Group.png")}
-                  className="group"
-                  alt="Tech 1"
-                />
-                <div className="bgimg1">
-                  <img
-                    src={require("../../src/assets/cards/ic4.png")}
-                    alt="Tech 1"
-                  />
-                </div>
-              </div>
-              <h5 className="m-2 ">Enterprise-Grade Security</h5>
-              <p className="tech-text">
-                <span >Protects </span>Protects user data with full encryption and compliance
-              </p>
-            </div>
-          </div>{" "}
-          <div className="col">
-            <div className="tech-card d-flex flex-column align-items-center text-center p-4">
-              <div className="img1">
-                <img
-                  src={require("../../src/assets/cards/Group.png")}
-                  className="group"
-                  alt="Tech 1"
-                />
-                <div className="bgimg1">
-                  <img
-                    src={require("../../src/assets/cards/ic5.png")}
-                    alt="Tech 1"
-                  />
-                </div>
-              </div>
-              <h5 className="m-2 ">Easy Customization</h5>
-              <p className="tech-text">
-                <span >Integrate Nouf </span> in minutes, no complex setup. Works seamlessly with web and mobile apps instantly.
-              </p>
-            </div>
-          </div>{" "}
-          <div className="col">
-            <div className="tech-card d-flex flex-column align-items-center text-center p-4">
-              <div className="img1">
-                <img
-                  src={require("../../src/assets/cards/Group.png")}
-                  className="group"
-                  alt="Tech 1"
-                />
-                <div className="bgimg1">
-                  <img
-                    src={require("../../src/assets/cards/ic6.png")}
-                    alt="Tech 1"
-                  />
-                </div>
-              </div>
-              <h5 className="m-2 ">Plug-and-Play API</h5>
-              <p className="tech-text">
-                <span >Track</span>  interactions, engagement, and conversions easily
-              </p>
-            </div>
-          </div>
+          ))}
+          
         </div>
       </div>
     </div>
